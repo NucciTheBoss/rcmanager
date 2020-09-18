26fa9411c65ce20ce97ae27b608617fc13aa7bd5
NucciTheBoss <nucci.programming@gmail.com>
2020-09-18 17:33:54 -0400

    Started writing upload

--- rcmanager.py
+++ rcmanager.py
@@ -89,6 +89,93 @@ def checkdatabase():
             conn.close()
 
 
+def toblob(filepath):
+    if os.path.exists(filepath):
+        fin = open(filepath, "rb")
+        tmp_blob = fin.read()
+        return tmp_blob
+
+    else:
+        return "err"
+
+
+def underlength(string, max_length):
+    if len(string) <= max_length:
+        return True
+
+    else:
+        return False
+
+
+# Rc file class to encapsulate rc file data
+class RCfile:
+    def __init__(self, name, shell, content, note):
+        if underlength(name, 20):
+            self.name = name
+
+        else:
+            print("{}: Over 20 characters. Please shorten the name".format(name))
+            exit()
+
+        self.shell = shell
+
+        # Verify that content is a valid rc file
+        tmp_content = toblob(content)
+        if tmp_content == "err":
+            print("{}: Could not find rcfile. Please check your path".format(content))
+            exit()
+
+        else:
+            self.content = tmp_content  # BLOB of rcfile
+
+        if underlength(note, 100):
+            self.note = note  # A short description of the rcfile
+
+        else:
+            print("{}: Over 100 characters. Please shorten the note".format(note))
+            exit()
+
+    def getname(self):
+        return self.name
+
+    def getshell(self):
+        return self.shell
+
+    def getcontent(self):
+        return self.content
+
+    def getnote(self):
+        return self.note
+
+    def setname(self, new_name):
+        if underlength(new_name, 20):
+            self.name = new_name
+
+        else:
+            print("{}: Over 20 characters. Please shorten the name".format(new_name))
+            exit()
+
+    def setshell(self, new_shell):
+        self.shell = new_shell
+
+    def setcontent(self, new_content):
+        tmp_content = toblob(new_content)
+        if tmp_content == "err":
+            print("{}: Could not find rcfile. Please check your path".format(new_content))
+            exit()
+
+        else:
+            self.content = tmp_content
+
+    def setnote(self, new_note):
+        if underlength(new_note, 100):
+            self.note = new_note  # A short description of the rcfile
+
+        else:
+            print("{}: Over 100 characters. Please shorten the note".format(new_note))
+            exit()
+
+
 # TODO: Write upload method
 # TODO: Write remove method
 # TODO: Write update method
@@ -103,9 +190,42 @@ def rcmanager():
 
 
 @rcmanager.command()
-def upload():
+@click.option("-n", "--name", default=None, help="The rc file's name")
+@click.option("-s", "--shell", type=click.Choice(["bash", "csh", "ksh",
+                                                  "tcsh", "zsh", "fish"],
+                                                 case_sensitive=False),
+              default=None,
+              help="The rc file's corresponding shell")
+@click.option("-f", "--rcfile", default=None, help="Path to the rc file")
+@click.option("--note", default=None, help="A short description of the rc file")
+@click.option("--yml_file", default=None, help="Upload rc file from YAML file")
+@click.option("-y", "--yes", is_flag=True)
+def upload(name, shell, rcfile, note, yml_file, yes):
     """Upload a new rc file to the rc file database."""
     checkdatabase()
+    # Begin the upload process
+    if yml_file is not None:
+        pass
+
+    else:
+        if name is None:
+            print("Please specify a name for your rc file")
+            exit()
+
+        elif shell is None:
+            print("Please specify a shell for your rcfile")
+            exit()
+
+        elif rcfile is None:
+            print("Please specify the path to your rcfile")
+            exit()
+
+        else:
+            # Verify if user create a note for the rc file
+            if note is None:
+                note = "null"
+
+        # TODO: Finish writing the upload part
 
 
 @rcmanager.command()
@@ -142,6 +262,11 @@ def query():
 def skel():
     """Manage skeleton rc files."""
     checkdatabase()
+
+
+@rcmanager.command()
+def swap():
+    """Swap rc files."""
 
 
 main = click.CommandCollection(sources=[rcmanager])
